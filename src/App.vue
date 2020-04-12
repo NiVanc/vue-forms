a18a8a5eba3ef60b1c156859bf3bd4575bc06f3e
NiVanc <nico@izebra.be>
2020-04-12 11:53:05 +0200

    Submit a form

--- src/App.vue
+++ src/App.vue
@@ -70,12 +70,12 @@
       <hr />
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button class="btn btn-primary">Submit!</button>
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -118,8 +118,14 @@ export default {
       sendMail: [],
       selectedPriority: "",
       priorities: ["Low", "Medium", "High"],
-      dataSwitch: true
+      dataSwitch: true,
+      isSubmitted: false
     };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    }
   },
   components: {
     appSwitch: Switch
